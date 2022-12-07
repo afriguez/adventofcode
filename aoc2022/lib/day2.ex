d0b35e30a96044e98bd0bbb283e2b2070b18fa24
Fer L <afriguez@gmail.com>
2022-12-07 12:41:14 -0500

    feat: add day2 part2

--- aoc2022/lib/day2.ex
+++ aoc2022/lib/day2.ex
@@ -1,15 +1,13 @@
 defmodule Day2 do
-  def calculate_score() do
+  def input() do
     {:ok, input} = File.read("./lib/day2.input")
 
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(&score/1)
-    |> Enum.sum()
   end
 
-  def score(match) do
-    case match do
+  def score(round) do
+    case round do
       "A X" -> 4
       "A Y" -> 8
       "A Z" -> 3
@@ -20,5 +18,30 @@ defmodule Day2 do
       "C Y" -> 2
       "C Z" -> 6
     end
+  end
+
+  def part_one() do
+    input()
+    |> Enum.map(&score/1)
+    |> Enum.sum()
+  end
+
+  def part_two() do
+    input()
+    |> Enum.map(
+      &case &1 do
+        "A X" -> "A Z"
+        "A Y" -> "A X"
+        "A Z" -> "A Y"
+        "B X" -> "B X"
+        "B Y" -> "B Y"
+        "B Z" -> "B Z"
+        "C X" -> "C Y"
+        "C Y" -> "C Z"
+        "C Z" -> "C X"
+      end
+    )
+    |> Enum.map(&score/1)
+    |> Enum.sum()
   end
 end
