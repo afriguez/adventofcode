f9cb432486ad2e2ef79bd7f1edf672a1bd6e83a2
Fer L <afriguez@gmail.com>
2022-12-07 22:02:46 -0500

    feat: add day3 part_two

--- aoc2022/lib/day3.ex
+++ aoc2022/lib/day3.ex
@@ -1,9 +1,22 @@
 defmodule Day3 do
-  def input() do
+  def part_one() do
     {:ok, input} = File.read("./lib/day3.input")
 
     String.split(input)
     |> Enum.map(&(&1 |> split() |> find_common |> priority()))
+    |> Enum.sum()
+  end
+
+  def part_two() do
+    {:ok, input} = File.read("./lib/day3.input")
+
+    chunks =
+      String.split(input, "\n", trim: true)
+      |> Enum.map(&String.split(&1, "", trim: true))
+      |> Enum.chunk_every(3)
+
+    Enum.map(chunks, &find_badge/1)
+    |> Enum.map(&priority/1)
     |> Enum.sum()
   end
 
@@ -15,6 +28,8 @@ defmodule Day3 do
   def priority(<<cp::utf8>>) when cp in ?A..?Z, do: cp - ?A + 27
 
   def priority(<<cp::utf8>>) when cp in ?a..?z, do: cp - ?a + 1
+
+  def find_badge([a, b, c]), do: hd((a -- a -- b) -- (a -- a -- b) -- c)
 
   def find_common({a, b}), do: hd(a -- a -- b)
 end
