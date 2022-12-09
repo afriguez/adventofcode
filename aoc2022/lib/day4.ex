aaa6eac0aaa1c4a0ee3c742d00d03aa5977067e3
Fer L <afriguez@gmail.com>
2022-12-08 23:17:36 -0500

    feat: add day4 part_two

--- aoc2022/lib/day4.ex
+++ aoc2022/lib/day4.ex
@@ -1,11 +1,22 @@
 defmodule Day4 do
-  def part_one() do
+  def input() do
     {:ok, input} = File.read("./lib/day4.input")
 
     String.split(input, "\n", trim: true)
     |> Enum.map(&split_and_convert/1)
+  end
+
+  def part_one() do
+    input()
     |> Enum.count(fn [l1, l2, r1, r2] ->
       (l2 <= r2 and l1 >= r1) or (l1 <= r1 and l2 >= r2)
+    end)
+  end
+
+  def part_two() do
+    input()
+    |> Enum.count(fn [l1, l2, r1, r2] ->
+      !Range.disjoint?(l1..l2, r1..r2)
     end)
   end
 
