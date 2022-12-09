9a3d2330adb83946b9ad6043ea817ee0a666ab70
Fer L <afriguez@gmail.com>
2022-12-08 22:49:46 -0500

    feat: add day4 and day4.input

---
+++ aoc2022/lib/day4.ex
@@ -0,0 +1,16 @@
+defmodule Day4 do
+  def part_one() do
+    {:ok, input} = File.read("./lib/day4.input")
+
+    String.split(input, "\n", trim: true)
+    |> Enum.map(&split_and_convert/1)
+    |> Enum.count(fn [l1, l2, r1, r2] ->
+      (l2 <= r2 and l1 >= r1) or (l1 <= r1 and l2 >= r2)
+    end)
+  end
+
+  def split_and_convert(line) do
+    String.split(line, [",", "-"])
+    |> Enum.map(&String.to_integer/1)
+  end
+end
