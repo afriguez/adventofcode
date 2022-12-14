ce206f12d042da5f7d4995bc05f6aa243dd3abe1
Fer L <afriguez@gmail.com>
2022-12-13 21:06:34 -0500

    feat: add day6 part two

--- aoc2022/lib/day6.ex
+++ aoc2022/lib/day6.ex
@@ -2,11 +2,17 @@ defmodule Day6 do
   def part_one() do
     {:ok, input} = File.read("./lib/day6.input")
 
-    find_four(input, 0)
+    find_marker(4, input, 0)
   end
 
-  def find_four(str, i) do
-    {marker, tail} = String.split_at(str, 4)
+  def part_two() do
+    {:ok, input} = File.read("./lib/day6.input")
+
+    find_marker(14, input, 0)
+  end
+
+  def find_marker(marker_length, str, i) do
+    {marker, tail} = String.split_at(str, marker_length)
     tail = String.slice(marker, 1..-1) <> tail
 
     size =
@@ -16,8 +22,8 @@ defmodule Day6 do
       |> MapSet.size()
 
     cond do
-      size == 4 -> i + 4
-      true -> find_four(tail, i + 1)
+      size == marker_length -> i + marker_length
+      true -> find_marker(marker_length, tail, i + 1)
     end
   end
 end
